1818027e5577dc40d545dac5422d74588aaf1c9b
mechamlogan <mechamlogan@gmail.com>
2018-07-20 18:34:12 -0600

    added icons with styles

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
+import Icon from '../icon';
+
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label className="library-course">Problem Solving</label>
-                {/* icon component */}
+                { Icon('fas fa-check', 'library-course__icon') }
                 {/* arrow component */}
-                {/* action button component */}
+                { Icon ('fas fa-plus-circle', 'library_course__action') }
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Bacon ipsum dolor amet turkey beef pork loin pork ball tip. Prosciutto alcatra shank, meatloaf ball tip porchetta jerky ham spare ribs venison. Pastrami spare ribs short loin sirloin.</p>
