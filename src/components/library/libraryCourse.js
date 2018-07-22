9366b7306de6e9c6ddd3cd79b6e04df244c90eed
mechamlogan <mechamlogan@gmail.com>
2018-07-21 20:12:50 -0600

    added action.js to application, library course component grid finished

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 
 import Icon from '../icon';
 import Arrow from '../arrow';
+import Action from '../action';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <div className="library-course__title-arrow">
+                <div className="library-course__title-check">
                     <label className="library-course__title">Problem Solving</label>
                     {Icon('fas fa-check', 'library-course__icon')}
                 </div>
                 <Arrow className="library-course__arrow" />
-
+                <Action className="library-course__action" />
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Bacon ipsum dolor amet turkey beef pork loin pork ball tip. Prosciutto alcatra shank, meatloaf ball tip porchetta jerky ham spare ribs venison. Pastrami spare ribs short loin sirloin.</p>
