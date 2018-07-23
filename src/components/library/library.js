6cabd0c58faef22bbc7cbc8a5d1ad2896bf5c65d
mechamlogan <mechamlogan@gmail.com>
2018-07-23 11:59:43 -0600

    FINISH VIDEO MAPPING STATE TO PROPS IN REDUX

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -6,21 +6,30 @@ import LibraryCourse from './libraryCourse';
 
 class Library extends Component {
 
-    componentDidMount() {
+    componentWillMount() {
         this.props.fetchCourses()
     }
 
+    renderCourses() {
+        const data = this.props.courses
+
+        return data.map((object))
+    }
+
     render() {
-        // this.props.fetchCourses()
         return (
             <div className="library">
                 <h1 className="library__title">Course Library</h1>
-                <LibraryCourse />
-                <LibraryCourse />
-                <LibraryCourse />
+                { this.renderCourses() }
             </div>
         )
     }
 }
 
-export default connect(null, actions)(Library);
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps, actions)(Library);
