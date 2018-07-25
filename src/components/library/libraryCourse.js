c0dba5f5754c434e8e4d07930c401a33e86a711f
mechamlogan <mechamlogan@gmail.com>
2018-07-25 08:36:21 -0600

    mapped course  object datalibrary course component instances

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -9,14 +9,16 @@ class LibraryCourse extends Component {
         return (
             <div className="library-course">
                 <div className="library-course__title-check">
-                    <label className="library-course__title"><h1>Problem Solving</h1></label>
+                    <label className="library-course__title"><h1>{ this.props.title }</h1></label>
                     {Icon('fas fa-check', 'library-course__icon')}
                 </div>
                 <Arrow className="library-course__arrow" />
                 <Action className="library-course__action" />
                 <div className="library-course__description">
                     <label>Course Description</label>
-                    <p>Bacon ipsum dolor amet turkey beef pork loin pork ball tip. Prosciutto alcatra shank, meatloaf ball tip porchetta jerky ham spare ribs venison. Pastrami spare ribs short loin sirloin.</p>
+                    <p>
+                        { this.props.description }
+                    </p>
                 </div>
             </div>
         )
