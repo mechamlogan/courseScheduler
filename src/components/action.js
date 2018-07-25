4c69871b1ef30b1cdc41f90cdf177c1cd1d4a33e
mechamlogan <mechamlogan@gmail.com>
2018-07-25 11:13:49 -0600

    styled library course component

--- src/components/action.js
+++ src/components/action.js
@@ -5,7 +5,7 @@ class Action extends Component {
     render() {
         return (
             <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                {Icon('fas fa-plus-circle','library-course__icon')}
+                {Icon('fas fa-plus-circle')}
                 {/* fas fa-times-circle */}
             </button>
         )
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -11,9 +11,10 @@ class LibraryCourse extends Component {
         return (
             <div className="library-course">
                 <div className="library-course__title-check">
-                    <label className="library-course__title"><h2>{ this.props.title }</h2></label>
+                    <label className="library-course__title">{ this.props.title }</label>
                     {Icon('fas fa-check', 'library-course__icon')}
                 </div>
+                <div className="library-course__line"></div>
                 <Arrow className="library-course__arrow" />
                 <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action" />
                 <div className="library-course__description">
